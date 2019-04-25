d088ffdbd5cb1e21f10fb9582bcd68e20d6e2448
Claudio <c.novella.rausell@gmail.com>
2019-04-25 10:46:06 +0200

    Trimmomatic script

--- code/08_Trimmomatic_25-04-2019.py
+++ code/08_Trimmomatic_25-04-2019.py
@@ -12,4 +12,4 @@ for fileR1 in os.listdir('./Serum'):
             log1 = dividing[0]
             output1 = dividing[0]
             output2 = dividing1[0]
-            subprocess.call("trimmomatic PE -threads 10 -phred33 " + './Serum' + fileR1 + " " + './Serum' + fileR2 + " " + processed + output1 + "_trimmed.fastq.gz " + "output_forward_unpaired.fq.gz " + processed + output2 + "_trimmed.fastq.gz " + "output_reverse_unpaired.fq.gz " + " LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:35" + " >" + log + log1 + "_trimmomatic.txt" + " 2>&1", shell=True)
+            subprocess.call("trimmomatic PE -threads 10 -phred33 " + './Serum' + fileR1 + " " + './Serum' + fileR2 + " " + '/home/clno2215/stunning-garbanzo/results/transcriptomics/Serum/Trimmomatic' + output1 + "_trimmed.fastq.gz " + "output_forward_unpaired.fq.gz " + '/home/clno2215/stunning-garbanzo/results/transcriptomics/Serum/Trimmomatic' + output2 + "_trimmed.fastq.gz " + "output_reverse_unpaired.fq.gz " + " LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:35" + " >" + '/home/clno2215/stunning-garbanzo/results/transcriptomics/Serum/trim_logs' + log1 + "_trimmomatic.txt" + " 2>&1", shell=True)
