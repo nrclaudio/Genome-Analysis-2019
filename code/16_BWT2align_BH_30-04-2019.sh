bb0335700318231a79550cec8d4f76f54ab7b3f0
Slakky <c.novella.rausell@gmail.com>
2019-05-01 02:49:43 +0200

    added scripts

---
+++ code/16_BWT2align_BH_30-04-2019.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 05:00:00
+#SBATCH -J BWT2alignBH_30-04-2019
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user c.novella.rausell@gmail.com
+# Load modules
+module load bioinfo-tools
+module load bowtie2
+# Your commands
+cd /home/clno2215/stunning-garbanzo/results/transcriptomics/BH/bowtie2
+bowtie2 -x /home/clno2215/stunning-garbanzo/data/transcriptomics/RNA-seq/metadata/bowtie2_index/efaec_pilon -1 /home/clno2215/stunning-garbanzo/data/transcriptomics/RNA-seq/trimmed_data/BH/trim_paired_ERR1797972_pass_1_paired.fq,/home/clno2215/stunning-garbanzo/data/transcriptomics/RNA-seq/trimmed_data/BH/trim_paired_ERR1797973_pass_1_paired.fq,/home/clno2215/stunning-garbanzo/data/transcriptomics/RNA-seq/trimmed_data/BH/trim_paired_ERR1797974_pass_1_paired.fq.gz -2 /home/clno2215/stunning-garbanzo/data/transcriptomics/RNA-seq/trimmed_data/BH/trim_paired_ERR1797972_pass_2_paired.fq,/home/clno2215/stunning-garbanzo/data/transcriptomics/RNA-seq/trimmed_data/BH/trim_paired_ERR1797973_pass_2_paired.fq,/home/clno2215/stunning-garbanzo/data/transcriptomics/RNA-seq/trimmed_data/BH/trim_paired_ERR1797974_pass_2_paired.fq.gz -S efaec_pilon_bwt2_30-04-2019.sam
