30f4bae734c36870e3494a6060d0b53a45183a93
Claudio <c.novella.rausell@gmail.com>
2019-05-01 15:46:46 +0200

    work

---
+++ code/18_BWT2align_Serum_30-04-2019.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 05:00:00
+#SBATCH -J BWT2alignSerum_30-04-2019
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user c.novella.rausell@gmail.com
+# Load modules
+module load bioinfo-tools
+module load bowtie2
+# Your commands
+cd /home/clno2215/stunning-garbanzo/results/transcriptomics/Serum/bowtie2
+bowtie2 -x /home/clno2215/stunning-garbanzo/data/transcriptomics/RNA-seq/metadata/bowtie2_index/efaec_pilon -1 /home/clno2215/projectdata/transcriptomics_data/RNA-Seq_Serum/trim_paired_ERR1797969_pass_1.fastq.gz,/home/clno2215/projectdata/transcriptomics_data/RNA-Seq_Serum/trim_paired_ERR1797970_pass_1.fastq.gz,/home/clno2215/stunning-garbanzo/data/transcriptomics/RNA-seq/trimmed_data/Serum/trim_paired_ERR1797971_pass_1.fastq.gz -2 /home/clno2215/stunning-garbanzo/data/transcriptomics/RNA-seq/trimmed_data/Serum/trim_paired_ERR1797969_pass_2.fastq.gz,/home/clno2215/stunning-garbanzo/data/transcriptomics/RNA-seq/trimmed_data/Serum/trim_paired_ERR1797970_pass_2.fastq.gz,/home/clno2215/stunning-garbanzo/data/transcriptomics/RNA-seq/trimmed_data/Serum/trim_paired_ERR1797971_pass_2.fastq.gz -S efaec_pilon_bwt2_30-04-2019.sam
+
