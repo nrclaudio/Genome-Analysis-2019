bde0e1baebc09b11a4aeee9d5da932a5139c0489
Claudio <c.novella.rausell@gmail.com>
2019-05-02 13:47:53 +0200

    added Samtools merge script

---
+++ code/22_STMerge_02-05-2019.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 05:00:00
+#SBATCH -J SamTools_02-05-2019
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user c.novella.rausell@gmail.com
+# Load modules
+module load bioinfo-tools
+module load samtools/1.9
+# Your commands
+cd /home/clno2215/stunning-garbanzo/results/transcriptomics/BH/bowtie2
+samtools merge merged.bam *.bam
+cd /home/clno2215/stunning-garbanzo/results/transcriptomics/Serum/bowtie2
+samtools merge merged.bam *.bam
